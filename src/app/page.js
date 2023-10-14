7f60610a07dcfdc87668b51b266ee0190c956843
Mrinmay <mrinmaydas2604@gmail.com>
2023-10-14 20:53:30 +0530

    bug fixes

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Hero from '../../components/Hero';
 
 export default function Home() {
   return (
-    <div className={styles.body}>
+    <div className={styles}>
     <Hero/>
     </div>    
   );
